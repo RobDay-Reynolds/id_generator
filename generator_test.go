eb7c4e0a9a257da82c884df9dc9da47fe84fd6d6
Rob Day-Reynolds <rdayreynolds@newrelic.com>
2021-05-24 00:11:09 -0700

    Add test confirming uniqueness across threads

--- generator_test.go
+++ generator_test.go
@@ -22,8 +22,38 @@ func TestGeneratorPerformance(t *testing.T) {
 
 	// If it takes more than a second then it wouldn't be performant enough
 	if timeTaken.Seconds() > 1 {
-		t.Errorf("Took too long to generate 100,000 IDs. Had to take less than one second, actually took %v", timeTaken)
+		t.Errorf("Took too long to generate 100000 IDs. Had to take less than one second, actually took %v", timeTaken)
 	}
 
-	fmt.Printf("Creating 100,000 IDs took %v\n", timeTaken)
+	fmt.Printf("Creating 100000 IDs took %v\n", timeTaken)
+}
+
+func TestGeneratorInParallel(t *testing.T) {
+	generator := NewGenerator()
+
+	consumer := make(chan uint64)
+
+	const idCount = 10000
+	generateIDs := func() {
+		for i:= 0; i < idCount; i++ {
+			consumer <- generator.NextID()
+		}
+	}
+
+	// Spin up five go funcs to create IDs in parallel
+	const numThreads = 5
+	for i := 0; i < numThreads; i++ {
+		go generateIDs()
+	}
+
+	// Read off of the channel and check for duplicates
+	idMap := map[uint64]bool{}
+	for i := 0; i < idCount*numThreads; i++ {
+		id := <-consumer
+		if _, found := idMap[id]; found {
+			t.Errorf("Duplicated ID")
+		}
+	}
+
+	fmt.Printf("Created %v unique IDs\n", idCount*numThreads)
 }
